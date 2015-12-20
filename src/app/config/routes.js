5191ca4371c856cdedde8fe63795673429b4aa96
Washington Silva <w-osilva@hotmail.com>
2015-12-20 21:09:13 -0200

    Adicionando configuração de rotas

---
+++ src/app/config/routes.js
@@ -0,0 +1,20 @@
+angular.module("MeuApp")
+
+    .config(['$routeProvider', function($routeProvider) {
+        $routeProvider.
+            when('/', {
+                templateUrl: 'src/app/views/home.html',
+                controller: 'homeCtrl'
+            }).
+            when('/filmes', {
+                templateUrl: 'src/app/views/filmes.html',
+                controller: 'filmesCtrl'
+            }).
+            when('/hello-world', {
+                templateUrl: 'src/app/views/hello-world.html'
+            }).
+            otherwise({
+                redirectTo: '/'
+            });
+        }
+    ])
